9a62f24845d1e385ad1d601cac4600873e195995
DeepanshuPratik <deepanshu.pratik@gmail.com>
2022-01-23 02:24:06 +0530

    Setting Up Android Studio for App

---
+++ App/InsCon/app/src/main/java/com/example/inscon/clubs/TrainingnPlacement.kt
@@ -0,0 +1,12 @@
+package com.example.inscon.clubs
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.example.inscon.R
+
+class TrainingnPlacement : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_trainingn_placement)
+    }
+}
