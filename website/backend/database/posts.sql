87642f340f84b6dce83f1e6c0435daf2b86a2f96
Soumya Ranjan Patnaik <soumyaranjan1812@gmail.com>
2022-02-05 13:41:12 +0530

    Restructured Project

--- website/backend/database/posts.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS user_post (
-    post_id varchar NOT NULL PRIMARY KEY,
-    photo_url varchar NOT NULL,
-    caption varchar,
-    user_id varchar NOT NULL,
-    CONSTRAINT fk_customer FOREIGN KEY(user_id) REFERENCES user_profile(user_id)
-);
-
-
--- website/backend/database/users.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS user_profile (
-    user_id varchar NOT NULL PRIMARY KEY,
-    email varchar NOT NULL UNIQUE,
-    password_hash varchar NOT NULL,
-    first_name varchar NOT NULL,
-    last_name varchar,
-    photo_url varchar
-);
