58190272aa7cbbbfebe9dc20aeeaa6e7d7bbbe88
DeepanshuPratik <deepanshu.pratik@gmail.com>
2022-02-05 16:03:59 +0530

    added ecell layouts and customized!

--- android/SocialIIITNR/app/src/main/java/com/iiitnr/social/ui/fragments/pages/Ecell.kt
+++ android/SocialIIITNR/app/src/main/java/com/iiitnr/social/ui/fragments/pages/Ecell.kt
@@ -5,10 +5,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.iiitnr.social.databinding.FragmentEcellBinding
 import com.iiitnr.social.databinding.FragmentTrainingnPlacementBinding
+import com.iiitnr.social.ui.Adapters.EcellRecyclerAdapter
 
 class Ecell : Fragment() {
+    private var layoutManager: RecyclerView.LayoutManager? = null
+    private var adapter: RecyclerView.Adapter<EcellRecyclerAdapter.ViewHolder>? = null
     private var _binding: FragmentEcellBinding? = null
     private val binding get() = _binding!!
 
@@ -18,5 +23,16 @@ class Ecell : Fragment() {
     ): View {
         _binding = FragmentEcellBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.ongoingEcellList.apply {
+            // set a LinearLayoutManager to handle Android
+            // RecyclerView behavior
+            layoutManager = LinearLayoutManager(activity)
+            // set the custom adapter to the RecyclerView
+            adapter = EcellRecyclerAdapter()
+        }
     }
 }
