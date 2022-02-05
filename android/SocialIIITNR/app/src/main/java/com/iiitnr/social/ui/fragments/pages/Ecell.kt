6b1745c67a8857c35ed1e49d75eec300a5c9bc43
DeepanshuPratik <deepanshu.pratik@gmail.com>
2022-02-05 20:36:35 +0530

    ecell done

--- android/SocialIIITNR/app/src/main/java/com/iiitnr/social/ui/fragments/pages/Ecell.kt
+++ android/SocialIIITNR/app/src/main/java/com/iiitnr/social/ui/fragments/pages/Ecell.kt
@@ -1,5 +1,6 @@
 package com.iiitnr.social.ui.fragments.pages
 
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -7,13 +8,15 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.iiitnr.social.R
 import com.iiitnr.social.databinding.FragmentEcellBinding
 import com.iiitnr.social.databinding.FragmentTrainingnPlacementBinding
 import com.iiitnr.social.ui.Adapters.EcellRecyclerAdapter
+import com.iiitnr.social.ui.Adapters.ImageViewHolder
 
 class Ecell : Fragment() {
     private var layoutManager: RecyclerView.LayoutManager? = null
-    private var adapter: RecyclerView.Adapter<EcellRecyclerAdapter.ViewHolder>? = null
+    private var adapter: RecyclerView.Adapter<ImageViewHolder>? = null
     private var _binding: FragmentEcellBinding? = null
     private val binding get() = _binding!!
 
@@ -27,12 +30,17 @@ class Ecell : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val items : ArrayList<Int> = ArrayList()
+        items.add(R.drawable.bigbull)
+        items.add(R.drawable.index)
+        items.add(R.drawable.esummit)
+        items.add(R.drawable.case_study_competition1)
         binding.ongoingEcellList.apply {
             // set a LinearLayoutManager to handle Android
             // RecyclerView behavior
             layoutManager = LinearLayoutManager(activity)
             // set the custom adapter to the RecyclerView
-            adapter = EcellRecyclerAdapter()
+            adapter = EcellRecyclerAdapter(items)
         }
     }
 }
