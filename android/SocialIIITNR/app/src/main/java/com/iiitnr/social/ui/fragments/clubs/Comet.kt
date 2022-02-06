e5d4a210c600271fc8e32433d7826b5ff254b54a
DeepanshuPratik <deepanshu.pratik@gmail.com>
2022-02-06 18:22:12 +0530

    fixed bugs

--- android/SocialIIITNR/app/src/main/java/com/iiitnr/social/ui/fragments/clubs/Comet.kt
+++ android/SocialIIITNR/app/src/main/java/com/iiitnr/social/ui/fragments/clubs/Comet.kt
@@ -1,8 +1,23 @@
 package com.iiitnr.social.ui.fragments.clubs
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.iiitnr.social.databinding.FragmentClubsBinding
+import com.iiitnr.social.databinding.FragmentCometBinding
 import com.iiitnr.social.ui.fragments.BaseFragment
 
 class Comet : BaseFragment() {
+    private var _binding: FragmentCometBinding? = null
+    private val binding get() = _binding!!
 
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentCometBinding.inflate(inflater, container, false)
+        return binding.root
+    }
 }
